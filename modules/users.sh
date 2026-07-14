339cd79dd5484818b25177788e992005f6f4b33a
gitechcode-star <gitech.code@gmail.com>
2026-07-13 19:17:24 -0500

    Update users.sh

--- modules/users.sh
+++ modules/users.sh
@@ -1,1 +1,55 @@
 
+#!/bin/bash
+
+while true
+do
+clear
+
+echo "══════════════════════════════"
+echo "       USER MANAGER"
+echo "══════════════════════════════"
+echo
+echo "[1] Crear Usuario"
+echo "[2] Eliminar Usuario"
+echo "[3] Renovar Usuario"
+echo "[4] Usuarios Online"
+echo
+echo "[0] Regresar"
+echo
+
+read -p "Seleccione una opción: " opt
+
+case $opt in
+
+1)
+echo "Crear Usuario"
+read -p "ENTER..."
+;;
+
+2)
+echo "Eliminar Usuario"
+read -p "ENTER..."
+;;
+
+3)
+echo "Renovar Usuario"
+read -p "ENTER..."
+;;
+
+4)
+echo "Usuarios Online"
+read -p "ENTER..."
+;;
+
+0)
+break
+;;
+
+*)
+echo "Opción inválida"
+sleep 1
+;;
+
+esac
+
+done
