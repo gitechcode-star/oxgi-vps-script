8f31b2d0dcc49e4c1640e70135b4d221ce77e381
gitechcode-star <gitech.code@gmail.com>
2026-07-13 22:36:25 -0500

    Update ssh.sh

--- modules/ssh.sh
+++ modules/ssh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source /usr/local/oxgi/modules/color.sh
 source /usr/local/oxgi/modules/header.sh
 
 while true
@@ -7,19 +8,22 @@ do
 
 show_header
 
-echo "SSH MANAGER"
+echo -e "${GREEN}SSH MANAGER${NC}"
 echo
-echo " [1] Crear Usuario SSH"
-echo " [2] Eliminar Usuario SSH"
-echo " [3] Renovar Usuario SSH"
-echo " [4] Cambiar Contraseña"
-echo " [5] Usuarios Online"
-echo " [6] Lista de Usuarios"
-echo " [7] Eliminar Expirados"
+
+echo -e "${CYAN}[1]${NC} Crear Usuario SSH"
+echo -e "${CYAN}[2]${NC} Eliminar Usuario SSH"
+echo -e "${CYAN}[3]${NC} Renovar Usuario SSH"
+echo -e "${CYAN}[4]${NC} Cambiar Contraseña"
+echo -e "${CYAN}[5]${NC} Usuarios Online"
+echo -e "${CYAN}[6]${NC} Lista de Usuarios"
+echo -e "${CYAN}[7]${NC} Eliminar Expirados"
+
 echo
-echo " [0] Regresar"
+echo -e "${RED}[0]${NC} Regresar"
 echo
-echo "══════════════════════════════════════════════════════════════"
+
+echo -e "${BLUE}══════════════════════════════════════════════════════════════${NC}"
 echo
 
 read -p "Seleccione una opción: " opt
@@ -28,63 +32,70 @@ case $opt in
 
 1)
 clear
+show_header
+echo -e "${YELLOW}Crear Usuario SSH${NC}"
 echo
-echo "Función en desarrollo:"
-echo "Crear Usuario SSH"
+echo "Función en desarrollo..."
 echo
 read -p "ENTER para continuar..."
 ;;
 
 2)
 clear
+show_header
+echo -e "${YELLOW}Eliminar Usuario SSH${NC}"
 echo
-echo "Función en desarrollo:"
-echo "Eliminar Usuario SSH"
+echo "Función en desarrollo..."
 echo
 read -p "ENTER para continuar..."
 ;;
 
 3)
 clear
+show_header
+echo -e "${YELLOW}Renovar Usuario SSH${NC}"
 echo
-echo "Función en desarrollo:"
-echo "Renovar Usuario SSH"
+echo "Función en desarrollo..."
 echo
 read -p "ENTER para continuar..."
 ;;
 
 4)
 clear
+show_header
+echo -e "${YELLOW}Cambiar Contraseña${NC}"
 echo
-echo "Función en desarrollo:"
-echo "Cambiar Contraseña"
+echo "Función en desarrollo..."
 echo
 read -p "ENTER para continuar..."
 ;;
 
 5)
 clear
+show_header
+echo -e "${YELLOW}Usuarios Online${NC}"
 echo
-echo "Función en desarrollo:"
-echo "Usuarios Online"
+echo "Función en desarrollo..."
 echo
 read -p "ENTER para continuar..."
 ;;
 
 6)
 clear
+show_header
+echo -e "${YELLOW}Lista de Usuarios${NC}"
 echo
-echo "Función en desarrollo:"
-echo "Lista de Usuarios"
+echo "Función en desarrollo..."
 echo
 read -p "ENTER para continuar..."
 ;;
 
 7)
 clear
+show_header
+echo -e "${YELLOW}Eliminar Expirados${NC}"
 echo
-echo "Función en desarrollo:"
-echo "Eliminar Expirados"
+echo "Función en desarrollo..."
 echo
 read -p "ENTER para continuar..."
 ;;
@@ -95,7 +106,7 @@ break
 
 *)
 echo
-echo "Opción inválida"
+echo -e "${RED}Opción inválida${NC}"
 sleep 1
 ;;
 
