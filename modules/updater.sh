361799817e2d39128eb4e64028892318cc168485
gitechcode-star <gitech.code@gmail.com>
2026-07-13 20:02:36 -0500

    Update updater.sh

--- modules/updater.sh
+++ modules/updater.sh
@@ -12,8 +12,6 @@ echo
 if [ ! -d "$REPO_DIR/.git" ]; then
     echo "[ERROR] Repositorio Git no encontrado."
     echo
-    echo "OXGI debe instalarse mediante git clone."
-    echo
     read -p "ENTER para continuar..."
     exit 1
 fi
@@ -29,20 +27,33 @@ LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse origin/main)
 
 if [ "$LOCAL" = "$REMOTE" ]; then
-    echo "[OK] Ya tienes la última versión."
+    echo "[OK] Ya tienes la ultima version."
     echo
     read -p "ENTER para continuar..."
     exit 0
 fi
 
 echo "[+] Descargando cambios..."
-git pull origin main
-
-chmod +x "$REPO_DIR"/oxgi.sh
-chmod +x "$REPO_DIR"/modules/*.sh
-
 echo
-echo "[OK] Script actualizado correctamente."
-echo
+
+git reset --hard HEAD
+git clean -fd
+
+if git pull origin main; then
+
+    chmod +x "$REPO_DIR"/oxgi.sh
+    chmod +x "$REPO_DIR"/modules/*.sh
+
+    echo
+    echo "[OK] Script actualizado correctamente."
+    echo
+
+else
+
+    echo
+    echo "[ERROR] No se pudo actualizar."
+    echo
+
+fi
 
 read -p "ENTER para continuar..."
