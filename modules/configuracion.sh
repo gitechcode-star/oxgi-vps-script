f80430116766c11109c2b34e445ca4256420a84c
gitechcode-star <gitech.code@gmail.com>
2026-07-13 19:58:28 -0500

    Update configuracion.sh

--- modules/configuracion.sh
+++ modules/configuracion.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+CONFIG="/etc/oxgi/config.conf"
+
+[ -f "$CONFIG" ] && source "$CONFIG"
+
 while true
 do
 
 clear
 
-echo "══════════════════════════════"
-echo "      CONFIGURACION"
-echo "══════════════════════════════"
+echo "══════════════════════════════════════"
+echo "            CONFIGURACION"
+echo "══════════════════════════════════════"
+echo
+echo " Dominio : ${DOMAIN:-No Configurado}"
+echo
+echo " SSH      : $SSH_PORT,$SSH_PORT_ALT"
+echo " HTTP     : $HTTP_PORT"
+echo " HTTPS    : $HTTPS_PORT"
+echo " WS       : $WS_PORT"
+echo " DROPBEAR : $DROPBEAR_PORT"
+echo " BADVPN   : $BADVPN_PORT"
+echo
+echo "══════════════════════════════════════"
 echo
 echo " [1] Nginx Manager"
 echo " [2] SSL Manager"
@@ -15,30 +30,55 @@ echo " [3] WebSocket Manager"
 echo " [4] Dropbear Manager"
 echo " [5] BadVPN Manager"
 echo " [6] Firewall Manager"
-echo " [7] Proxy Manager"
-echo " [8] Puertos"
-echo " [9] Dominios"
+echo " [7] Puertos"
+echo " [8] Dominios"
 echo
 echo " [0] Regresar"
 echo
+echo "══════════════════════════════════════"
 
 read -p "Seleccione una opcion: " opt
 
 case $opt in
 
-1) bash /usr/local/oxgi/modules/config/nginx.sh ;;
-2) bash /usr/local/oxgi/modules/config/ssl.sh ;;
-3) bash /usr/local/oxgi/modules/config/websocket.sh ;;
-4) bash /usr/local/oxgi/modules/config/dropbear.sh ;;
-5) bash /usr/local/oxgi/modules/config/badvpn.sh ;;
-6) bash /usr/local/oxgi/modules/config/firewall.sh ;;
-7) bash /usr/local/oxgi/modules/config/proxy.sh ;;
-8) bash /usr/local/oxgi/modules/config/puertos.sh ;;
-9) bash /usr/local/oxgi/modules/config/dominios.sh ;;
+1)
+bash /usr/local/oxgi/modules/nginx.sh
+;;
 
-0) break ;;
+2)
+bash /usr/local/oxgi/modules/ssl.sh
+;;
+
+3)
+bash /usr/local/oxgi/modules/websocket.sh
+;;
+
+4)
+bash /usr/local/oxgi/modules/dropbear.sh
+;;
+
+5)
+bash /usr/local/oxgi/modules/badvpn.sh
+;;
+
+6)
+bash /usr/local/oxgi/modules/firewall.sh
+;;
+
+7)
+bash /usr/local/oxgi/modules/puertos.sh
+;;
+
+8)
+bash /usr/local/oxgi/modules/dominios.sh
+;;
+
+0)
+break
+;;
 
 *)
+echo
 echo "Opcion invalida"
 sleep 1
 ;;
