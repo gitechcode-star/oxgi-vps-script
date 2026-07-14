b5008211a3bc7ffe2f79faa058a6ae440a6a7b6e
gitechcode-star <gitech.code@gmail.com>
2026-07-13 19:16:54 -0500

    Update system.sh

--- modules/system.sh
+++ modules/system.sh
@@ -1,7 +1,57 @@
-
 #!/bin/bash
 
+while true
+do
 clear
-echo "SYSTEM MODULE"
+
+echo "══════════════════════════════"
+echo "         SYSTEM"
+echo "══════════════════════════════"
 echo
-read -p "Presione ENTER para volver..."
+echo "[1] Información del Sistema"
+echo "[2] Uso de RAM"
+echo "[3] Uso de Disco"
+echo "[4] Uptime"
+echo
+echo "[0] Regresar"
+echo
+
+read -p "Seleccione una opción: " opt
+
+case $opt in
+1)
+echo
+echo "SYSTEM INFO"
+read -p "ENTER para continuar..."
+;;
+
+2)
+echo
+echo "RAM INFO"
+read -p "ENTER para continuar..."
+;;
+
+3)
+echo
+echo "DISK INFO"
+read -p "ENTER para continuar..."
+;;
+
+4)
+echo
+echo "UPTIME INFO"
+read -p "ENTER para continuar..."
+;;
+
+0)
+break
+;;
+
+*)
+echo "Opción inválida"
+sleep 1
+;;
+
+esac
+
+done
