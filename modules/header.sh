eb511ac96ab6004e19f52e5d8a08c3802109487f
gitechcode-star <gitech.code@gmail.com>
2026-07-13 22:14:49 -0500

    Update header.sh

--- modules/header.sh
+++ modules/header.sh
@@ -8,11 +8,16 @@ VERSION_FILE="/etc/oxgi/version.conf"
 [ -f "$CONFIG" ] && source "$CONFIG"
 [ -f "$VERSION_FILE" ] && source "$VERSION_FILE"
 
+BLUE='\033[1;34m'
+CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m'
+
 clear
 
-echo "══════════════════════════════════════════════════════════════"
-echo "      $APP_NAME - Versión : $VERSION - ($AUTHOR)"
-echo "══════════════════════════════════════════════════════════════"
+echo -e "${BLUE}══════════════════════════════════════════════════════════════${NC}"
+echo -e "${WHITE}      ${APP_NAME} ${CYAN}- Versión ${VERSION} - (${AUTHOR})${NC}"
+echo -e "${BLUE}══════════════════════════════════════════════════════════════${NC}"
 echo
 
 }
