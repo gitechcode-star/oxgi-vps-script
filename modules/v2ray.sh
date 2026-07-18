cac2102ca4a65e00fbd7ed25fc61705f1f8b8389
gitechcode-star <gitech.code@gmail.com>
2026-07-18 00:22:51 -0500

    Update v2ray.sh

--- modules/v2ray.sh
+++ modules/v2ray.sh
@@ -2,5 +2,5 @@
 source /usr/local/oxgi/modules/color.sh
 source /usr/local/oxgi/modules/header.sh
 
-# Redirige directamente al gestor completo de usuarios que contiene la lógica SSH avanzada
+# Redirige al gestor completo (puedes expandir la opción 2 en users.sh para V2Ray específico)
 bash /usr/local/oxgi/modules/users.sh
